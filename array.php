1e4f8a0dd7fa174cb3669d158738d7c7b0f76429
ZunairaAnsari <zunairaansari413@gmail.com>
2024-07-31 23:37:33 +0500

    practising arrays

--- array.php
+++ array.php
@@ -16,8 +16,33 @@
     foreach ($myarray[2] as $fruits) {
         echo $fruits . "<br/>";
     }
+    
+    echo $myarray[1];
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+
+    
+    echo $myarray[0];
+    echo "<br/>";
+
+
+    // Remove "mango" from the nested array
+    unset($myarray[2][1]);
+
+    // Re-index the array to ensure correct indexing
+    $myarray[2] = array_values($myarray[2]);
+
+    // Display the results
+    echo $myarray[0] . "<br/>";
+    echo $myarray[1] . "<br/>";
+    foreach ($myarray[2] as $fruit) {
+        echo $fruit . "<br/>";
+    }
+
 
     ?>
+
 </body>
 
 </html>
