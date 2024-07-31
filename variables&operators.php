98083601387d656a632cbc59c8c869825764dc92
ZunairaAnsari <zunairaansari413@gmail.com>
2024-07-31 22:53:59 +0500

    practice huff

--- variables&operators.php
+++ variables&operators.php
@@ -1,5 +1,7 @@
 <?php
 
+// variables and plus minus operations
+
 $a = 2;
 $b = 5;
 
@@ -7,5 +9,13 @@ echo $a . ' ' . $b . "<br />";
 echo $a + $b . "<br />";
 echo $a - $b . "<br />";
 echo $a / $b . "<br />";
+
+// ternary operator
+
+$num = 13;
+
+$result = ($num % 2 == 0)? "Even" : "Odd";
+
+echo "The number $num is  $result";
 
 ?>
